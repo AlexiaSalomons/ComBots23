362b0527d7407367a146d2363d8314694a0868ec
Nielssss99 <94103117+Nielssss99@users.noreply.github.com>
2023-11-13 21:40:12 +0100

    Create example_identification.py

---
+++ example_identification.py
@@ -0,0 +1,4 @@
+# Dit kan alleen als we een werkende notebook hebben. Op deze manier kan je het id krijgen
+# for object in controller.last_event.metadata["objects"]:
+#   if object["visible"]:
+#     print(object['objectId'])
