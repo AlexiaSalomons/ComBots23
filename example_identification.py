5d2b8d1057887af7ca00eb840cb678642ce64aa9
AlexiaSalomons <99325711+AlexiaSalomons@users.noreply.github.com>
2023-12-12 13:05:01 +0100

    Delete example_identification.py

--- example_identification.py
+++
@@ -1,4 +0,0 @@
-# Dit kan alleen als we een werkende notebook hebben. Op deze manier kan je het id krijgen
-# for object in controller.last_event.metadata["objects"]:
-#   if object["visible"]:
-#     print(object['objectId'])
